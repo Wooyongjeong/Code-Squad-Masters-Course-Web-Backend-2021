b8a593662b50f189f860225d252fa9729e25dd1e
Wooyongjeong <dndyd1231@naver.com>
2020-12-08 15:39:05 +0900

    Modify push_word.py
    1단계에서 구현한 push_word.py에서 입력받는 부분과 출력하는 부분을 삭제하였습니다.
    
    2단계에서 push_word.py의 push_left(), push_right() 함수만을 사용하기 위함입니다.

--- push_word.py
+++ push_word.py
@@ -13,19 +13,3 @@ def push_right(num, word):
         return word[len(word)-num:] + word[:len(word)-num]
     else:
         return push_left(-num, word)
-
-# 입력 받기
-word, num, direction = input('> ').split()
-while int(num) < -100 or int(num) >= 100:
-    num = int(input('숫자는 -100보다 크거나 같고, 100보다 작아야합니다 > '))
-directions = ['L', 'R', 'l', 'r']
-while direction not in directions:
-    direction = input('방향은 L, R, l, r 중 하나여야 합니다 > ')
-
-# 출력하기
-if direction == 'L' or direction == 'l':
-    answer = push_left(int(num), word)
-    print(answer)
-elif direction == 'R' or direction == 'r':
-    answer = push_right(int(num), word)
-    print(answer)
