fb7732b3280a335ed07876a604387ae36d78f9a7
Wooyongjeong <dndyd1231@naver.com>
2020-12-08 15:54:27 +0900

    Add enter_commands() function
    plane_cube 클래스의 멤버 함수인 enter_commands() 함수를 추가하였습니다.
    
    문제의 요구사항대로 간단한 프롬프트인 (CUBE> )를 표시해주며 사용자로부터 입력을 받아 처리하는 로직을 작성하였습니다.
    
    주석을 추가하였습니다.

--- plane_cube.py
+++ plane_cube.py
@@ -7,9 +7,29 @@ class plane_cube:
         self.print_cube() # 현재 큐브 상태를 출력하는 함수
         self.enter_commands() # 사용자로부터 입력을 받아 처리하는 함수
     
+    # 현재 큐브 상태를 출력하는 함수
     def print_cube(self):
         for i in range(3):
             for j in range(3):
                 print(self.cube[i][j], end=' ')
             print()
         print()
+    
+    # 사용자로부터 입력을 받아 처리하는 함수
+    def enter_commands(self):
+        while True:
+            cmd = input('CUBE> ')
+            if cmd == 'Q':
+                print('Bye~')
+                break
+            for i in range(len(cmd)):
+                if cmd[i] == "'":
+                    continue
+                command = cmd[i]
+                if i + 1 != len(cmd) and cmd[i+1] == "'":
+                    command += "'"
+                if self.check(command) == True:
+                    self.process_command(command)
+                else:
+                    print(f"잘못된 입력입니다. 입력은 {self.valid_commands}만 가능합니다.")
+                    break
