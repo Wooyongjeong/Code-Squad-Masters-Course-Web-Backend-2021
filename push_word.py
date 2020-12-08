06cc4d1f795b84d9fb7854abdbfa76697e655acf
Wooyongjeong <dndyd1231@naver.com>
2020-12-08 14:44:11 +0900

    Modify push_word.py
    주석을 추가하였습니다.
    
    입력받는 부분과 출력하는 부분 사이에 공백을 넣어 가독성을 높였습니다.

--- push_word.py
+++ push_word.py
@@ -1,3 +1,4 @@
+# 왼쪽으로 밀기
 def push_left(num, word):
     if num > 0:
         num %= len(word)
@@ -5,6 +6,7 @@ def push_left(num, word):
     else:
         return push_right(-num, word)
         
+# 오른쪽으로 밀기
 def push_right(num, word):
     if num > 0:
         num %= len(word)
@@ -12,12 +14,15 @@ def push_right(num, word):
     else:
         return push_left(-num, word)
 
+# 입력 받기
 word, num, direction = input('> ').split()
 while int(num) < -100 or int(num) >= 100:
     num = int(input('숫자는 -100보다 크거나 같고, 100보다 작아야합니다 > '))
 directions = ['L', 'R', 'l', 'r']
 while direction not in directions:
     direction = input('방향은 L, R, l, r 중 하나여야 합니다 > ')
+
+# 출력하기
 if direction == 'L' or direction == 'l':
     answer = push_left(int(num), word)
     print(answer)
