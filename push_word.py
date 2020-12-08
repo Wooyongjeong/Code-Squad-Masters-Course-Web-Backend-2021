c511d77ec4ed191cb75b5a8927eb9123578189fd
Wooyongjeong <dndyd1231@naver.com>
2020-12-08 14:15:20 +0900

    Add push_left(), push_right() function
    입력한 단어를 왼쪽으로 밀어내는 함수인 push_left(), 오른쪽으로 밀어내는 함수인 push_right()를 추가하였습니다.

---
+++ push_word.py
@@ -0,0 +1,15 @@
+def push_left(num, word):
+    if num > 0:
+        word = list(word)
+        num %= len(word)
+        return ''.join(word[num:] + word[:num])
+    else:
+        return push_right(-num, word)
+        
+def push_right(num, word):
+    if num > 0:
+        word = list(word)
+        num %= len(word)
+        return ''.join(word[len(word)-num:] + word[:len(word)-num])
+    else:
+        return push_left(-num, word)
