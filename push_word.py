d8175458c2ec493cbfd89f22f7db10cccf33514f
Wooyongjeong <dndyd1231@naver.com>
2020-12-08 14:32:46 +0900

    Update push_left(), push_right() function
    문자열을 리스트로 만들어주는 작업이 필요없다고 판단하여 변경하였습니다.

--- push_word.py
+++ push_word.py
@@ -1,16 +1,14 @@
 def push_left(num, word):
     if num > 0:
-        word = list(word)
         num %= len(word)
-        return ''.join(word[num:] + word[:num])
+        return word[num:] + word[:num]
     else:
         return push_right(-num, word)
         
 def push_right(num, word):
     if num > 0:
-        word = list(word)
         num %= len(word)
-        return ''.join(word[len(word)-num:] + word[:len(word)-num])
+        return word[len(word)-num:] + word[:len(word)-num]
     else:
         return push_left(-num, word)
 
