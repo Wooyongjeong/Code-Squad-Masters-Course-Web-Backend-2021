6489136ccf6aca00794d981af55347578d399634
Wooyongjeong <dndyd1231@naver.com>
2020-12-10 18:58:45 +0900

    Modify "plane_cube.py"
    - plane_cube 클래스의 생성자에서 메소드 함수를 호출하는 부분을 삭제하였습니다.
    
    생성자에서 self.print_cube()와 self.enter_commands() 함수를 호출하는 부분을 삭제하였습니다.

--- plane_cube.py
+++ plane_cube.py
@@ -4,9 +4,7 @@ from push_word import push_left, push_right
 class plane_cube:
     def __init__(self):
         self.cube = [['R', 'R', 'W'], ['G', 'C', 'W'], ['G', 'B', 'B']] # 초기 큐브 상태
-        self.print_cube() # 현재 큐브 상태를 출력하는 함수
         self.valid_commands = ["U", "U'", "R", "R'", "L", "L'", "B", "B'", "Q"] # 유효한 명령어 리스트
-        self.enter_commands() # 사용자로부터 입력을 받아 처리하는 함수
     
     # 현재 큐브 상태를 출력하는 함수
     def print_cube(self):
