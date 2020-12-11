c0b7f13f6e0bffbd9bc817ff73aafea49964f0f0
Wooyongjeong <dndyd1231@naver.com>
2020-12-11 15:30:18 +0900

    Modify cube.py
    - 모듈 업데이트 하는 부분을 수정하였습니다.
    
    time.time()에서 time()으로 수정하였습니다.

--- cube.py
+++ cube.py
@@ -5,7 +5,7 @@ from random import randrange, choice
 
 class cube():
     def __init__(self):
-        self.start_time = time.time() # 경과 시간 측정을 위해 시간 측정
+        self.start_time = time() # 경과 시간 측정을 위해 시간 측정
         self.cube = [
             plane_cube('B').cube,
             plane_cube('W').cube, plane_cube('O').cube, plane_cube('G').cube, plane_cube('Y').cube,
@@ -255,7 +255,7 @@ class cube():
         while True:
             cmd = input('CUBE> ')
             if cmd == 'Q':
-                end_time = time.time()
+                end_time = time()
                 elapsed_time = int(end_time - self.start_time)
                 print(f"경과 시간: {elapsed_time // 60:02d}:{elapsed_time % 60:02d}")
                 print("이용해주셔서 감사합니다. 뚜뚜뚜.")
